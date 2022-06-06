ef8ad7c83925a6ec1dae3b8539c582130210e331
oly2o6 <calumroberts02@hotmail.com>
2022-06-06 07:21:14 +0100

    Removed fleshToLeather

--- kubejs/server_scripts/ATM/custom_recipes.js
+++ kubejs/server_scripts/ATM/custom_recipes.js
@@ -1,7 +1,7 @@
 onEvent(`recipes`, e => {
-  e.shapeless(`kubejs:rotten_leather`, [`minecraft:rotten_flesh`, `minecraft:rotten_flesh`, `minecraft:rotten_flesh`])
-  e.smelting(Item.of(`minecraft:leather`), `kubejs:rotten_leather`).xp(0.5)
-  e.recipes.minecraft.smoking(Item.of(`minecraft:leather`), `kubejs:rotten_leather`).xp(0.5)
+ // e.shapeless(`kubejs:rotten_leather`, [`minecraft:rotten_flesh`, `minecraft:rotten_flesh`, `minecraft:rotten_flesh`])
+//  e.smelting(Item.of(`minecraft:leather`), `kubejs:rotten_leather`).xp(0.5)
+ // e.recipes.minecraft.smoking(Item.of(`minecraft:leather`), `kubejs:rotten_leather`).xp(0.5)
 
   function customBlock(block, item) {
     e.shapeless(Item.of(item, 9), block)
