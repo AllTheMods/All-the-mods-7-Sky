0981cc594eddc4d5725173a0ef8277e6fe307b2c
enigmaquip <enigmaquip@users.noreply.github.com>
2022-07-01 15:32:38 -0600

    skyblock occultism iesnium tweaks

---
+++ kubejs/server_scripts/mod_specific/occultism/iesnium.js
@@ -0,0 +1,7 @@
+onEvent('recipes', event => {
+    event.custom({
+        'type':'occultism:spirit_fire',
+        'ingredient':{'item':'allthecompressed:netherrack_block_1x'},
+        'result':{'item':'occultism:iesnium_ore_natural'}
+    })
+})
