63cea54c68c1f06d42993a75c34bb544aa386f85
oly2o6 <calumroberts02@hotmail.com>
2022-07-02 02:53:04 +0100

    magical soil

--- kubejs/startup_scripts/custom_stuff.js
+++ kubejs/startup_scripts/custom_stuff.js
@@ -6,4 +6,5 @@ onEvent('item.registry', e => {
 onEvent('block.registry', e => {
 	e.create('fluorite_block').displayName('Fluorite Block').material('ice').hardness(1.2)
 	e.create('lithium_block').displayName('Lithium Block').material('iron').hardness(1)
+	e.create('magical_soil').displayName('§bMagical Soil').material('grass').hardness(0.6);
 })
