c18897b7176dd62160e26e6ac1971de9d1a9ee7e
KuroPeach <1193319600@qq.com>
2022-08-15 10:16:56 +0800

    Disable players open the Twilight Uncrafting_table
    Disable players open the Twilight Uncrafting_table, the uncrafting_table will be generated naturally, this is a long-standing problem, even if the usage is turned off in the configuration file, it will crash game when players put items in it.

--- kubejs/server_scripts/mod_specific/twilightforest/twilight.js
+++ kubejs/server_scripts/mod_specific/twilightforest/twilight.js
@@ -1,3 +1,9 @@
 onEvent('recipes', event => {
     event.remove({ id: 'twilightforest:uncrafting_table' })
 })
+
+onEvent('block.right_click', event => {
+  if (event.block.id == "twilightforest:uncrafting_table") {
+    event.cancel()
+  }
+})
