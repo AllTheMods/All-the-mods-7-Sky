2d3e9d56af31e5f6881c11e709f80bed364b7e38
enigmaquip <enigmaquip@users.noreply.github.com>
2022-08-17 17:21:31 -0600

    Add extra info to some solitary bee nests

--- kubejs/client_scripts/jei_info.js
+++ kubejs/client_scripts/jei_info.js
@@ -1,5 +1,8 @@
 onEvent('jei.information', event => {
     event.add('occultism:iesnium_ore',['Throwing Netherrack Block 1x in §5Spiritfire§r will turn it into Netherrack with Iesnium in it'])
+    event.add('productivebees:sand_nest', 'In this Skyblock, this nest will attract bees in any overworld biome.')
+    event.add('productivebees:snow_nest', 'In this Skyblock, this nest will attract bees in any overworld biome.')
+    event.add('productivebees:gravel_nest', 'In this Skyblock, this nest will attract bees in any overworld biome.')
 })
 onEvent('item.tooltip', event => {
     event.add('allthemodium:teleport_pad',[
