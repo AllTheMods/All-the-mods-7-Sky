2a6b42e587413d5c4d04664a26502318302389b5
enigmaquip <enigmaquip@users.noreply.github.com>
2022-08-30 08:06:32 -0600

    Clean up quartz dust recipes. Closes #118

--- kubejs/server_scripts/mod_specific/ae/ae.js
+++ kubejs/server_scripts/mod_specific/ae/ae.js
@@ -1,18 +1,17 @@
 onEvent('recipes', e => {
- e.remove({id: 'aeinfinitybooster:infinity_card'})
- e.shaped('aeinfinitybooster:infinity_card', ['EBE', 'BUB', 'NNN'], {
-    U: '#forge:ingots/unobtainium',
-    B: 'ae2:wireless_booster',
-    E: 'alltheores:enderium_plate',
-	N: 'minecraft:netherite_ingot'
-})
-	
-e.recipes.mekanismCrushing('thermal:quartz_dust', 'minecraft:quartz')
-e.recipes.immersiveengineeringCrusher('thermal:quartz_dust', 'minecraft:quartz')
-e.recipes.createCrushing('thermal:quartz_dust', 'minecraft:quartz')
-e.recipes.ftbic.macerating('thermal:quartz_dust', 'minecraft:quartz')
-e.smelting('ae2:silicon','#forge:dusts/quartz').xp(0.35)
-e.blasting('ae2:silicon','#forge:dusts/quartz').xp(0.35)
-	
-	
+    e.remove({ id: 'aeinfinitybooster:infinity_card' })
+    e.shaped('aeinfinitybooster:infinity_card', ['EBE', 'BUB', 'NNN'], {
+        U: '#forge:ingots/unobtainium',
+        B: 'ae2:wireless_booster',
+        E: 'alltheores:enderium_plate',
+        N: 'minecraft:netherite_ingot'
+    })
+
+    e.recipes.mekanismCrushing('thermal:quartz_dust', 'minecraft:quartz')
+    e.recipes.immersiveengineeringCrusher('thermal:quartz_dust', 'minecraft:quartz')
+    e.recipes.createCrushing('thermal:quartz_dust', 'minecraft:quartz')
+    e.recipes.ftbic.macerating('thermal:quartz_dust', 'minecraft:quartz')
+    e.smelting('ae2:silicon', '#forge:dusts/quartz').xp(0.35)
+    e.blasting('ae2:silicon', '#forge:dusts/quartz').xp(0.35)
+    e.remove({id:'mekanism:processing/quartz/to_dust'})
 })
