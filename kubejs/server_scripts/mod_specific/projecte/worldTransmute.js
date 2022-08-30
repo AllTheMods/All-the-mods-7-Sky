64598356d4ca10e03ec55e21873e582709fc2974
enigmaquip <enigmaquip@users.noreply.github.com>
2022-08-30 07:58:13 -0600

    Adds small flower rotation to in world Philo Stone transmuting

--- kubejs/server_scripts/mod_specific/projecte/worldTransmute.js
+++ kubejs/server_scripts/mod_specific/projecte/worldTransmute.js
@@ -3,12 +3,39 @@ const WorldTransmutation = java('moze_intel.projecte.utils.WorldTransmutations')
 onEvent('recipes', e => {
     WorldTransmutation.resetWorldTransmutations()
     // WorldTransmutation.register(BlockState original, BlockState result, (@Nullable) BlockState sneakResult)
-    let blue = Block.id("ars_nouveau:blue_archwood_sapling").getBlockState()
-    let purple = Block.id("ars_nouveau:purple_archwood_sapling").getBlockState()
-    let green = Block.id("ars_nouveau:green_archwood_sapling").getBlockState()
-    let red = Block.id("ars_nouveau:red_archwood_sapling").getBlockState()
-    WorldTransmutation.register(blue, purple, red)
-    WorldTransmutation.register(purple, green, blue)
-    WorldTransmutation.register(green, red, purple)
-    WorldTransmutation.register(red, blue, green)
+
+    function PhiloList(idList) {
+        for (i = 0; i < idList.length; i++) {
+            forward = (i + 1) >= idList.length ? 0 : (i + 1)
+            backward = (i - 1) < 0 ? idList.length - 1 : (i - 1)
+            WorldTransmutation.register(
+                Block.id(idList[i]).getBlockState(),
+                Block.id(idList[forward]).getBlockState(),
+                Block.id(idList[backward]).getBlockState()
+            )
+        }
+    }
+    let ars = [
+        'ars_nouveau:blue_archwood_sapling',
+        'ars_nouveau:purple_archwood_sapling',
+        'ars_nouveau:green_archwood_sapling',
+        'ars_nouveau:red_archwood_sapling'
+    ]
+    PhiloList(ars)
+    let smallflowers = [
+        'minecraft:dandelion',
+        'minecraft:poppy',
+        'minecraft:blue_orchid',
+        'minecraft:allium',
+        'minecraft:azure_bluet',
+        'minecraft:red_tulip',
+        'minecraft:orange_tulip',
+        'minecraft:white_tulip',
+        'minecraft:pink_tulip',
+        'minecraft:oxeye_daisy',
+        'minecraft:cornflower',
+        'minecraft:lily_of_the_valley',
+        'forbidden_arcanus:yellow_orchid'
+    ]
+    PhiloList(smallflowers)
 })
